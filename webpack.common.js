034d9ef28b958f12a002a21b3a0dd42ef390d454
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-10-09 21:12:13 +1300

    Install 'copy-webpack-plugin'
    Install 'copy-webpack-plugin' to use weatherIcons in production code.

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,15 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "assets" }],
     }),
   ],
   module: {
